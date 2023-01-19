1d375ee95ad27f03b3fb05beca19f317560c71a8
alhatmii <alhatmii.150@gmail.com>
2023-01-19 11:03:46 +0400

    First Commit

---
+++ src/Vehicle.java
@@ -0,0 +1,24 @@
+
+public class Vehicle {
+	private int make;
+	private int model;
+	private int year;
+	private String color;
+	private int numberOfWheels;
+	private double weight;
+
+	private void start() {
+	}
+
+	private void stop() {
+
+	}
+
+	private void drive() {
+
+	}
+
+	private void honk() {
+
+	}
+}
